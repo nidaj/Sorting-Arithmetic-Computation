f6cb461c7fe38855f6ca62d2f52c84eae7919657
nida.j <nida.jawre@gmail.com>
2020-11-02 20:35:55 +0530

    Added: Code to accept the value for a,b,c

--- SortingArthComp.sh
+++ SortingArthComp.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
 echo "This problem computes different arithmetic expressions and Sorts the results"
+read -p "Enter the value for a: " a
+read -p "Enter the value for b: " b
+read -p "Enter the value for c: " c
+
