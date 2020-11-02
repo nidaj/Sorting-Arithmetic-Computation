e6e2ead9a425cb21c4314d721724ea903e9d96ff
nida.j <nida.jawre@gmail.com>
2020-11-02 20:48:02 +0530

    Added: calculated first computation a + b * c

--- SortingArthComp.sh
+++ SortingArthComp.sh
@@ -3,4 +3,4 @@ echo "This problem computes different arithmetic expressions and Sorts the resul
 read -p "Enter the value for a: " a
 read -p "Enter the value for b: " b
 read -p "Enter the value for c: " c
-
+v1=`expr $a + $b \* $c`
